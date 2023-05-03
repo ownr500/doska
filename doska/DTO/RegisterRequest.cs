64e5a9809c14ae26493b11041067acdcb7e24c16
velvet_orange <ismediaspb@gmail.com>
2023-05-04 02:12:05 +0300

    some refactoring

--- doska/DTO/RegisterRequest.cs
+++ doska/DTO/RegisterRequest.cs
@@ -2,8 +2,8 @@
 
 public class RegisterRequest
 {
-    public string FirstName { get; set; } = default!;
-    public string LastName { get; set; } = default!;
-    public string Email { get; set; } = default!;
-    public string Password { get; set; } = default!;
+    public string FirstName { get; init; } = default!;
+    public string LastName { get; init; } = default!;
+    public string Email { get; init; } = default!;
+    public string Password { get; init; } = default!;
 }
