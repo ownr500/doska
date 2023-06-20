907d4714f0eba1bf8f2e2ed41e1e8d2eb758d6f2
velvet_orange <ismediaspb@gmail.com>
2023-06-20 14:22:44 +0300

    SigninRequest email validator

---
+++ doska/Validators/SinginRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using doska.DTO;
+using FluentValidation;
+
+namespace doska.Validators;
+
+public class SinginRequestValidator : AbstractValidator<SigninRequest>
+{
+    public SinginRequestValidator()
+    {
+        RuleFor(request => request.Email).EmailAddress();
+    }
+}
