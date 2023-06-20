50e20ff26e7f0edf495e5b16e29630071b71bc05
velvet_orange <ismediaspb@gmail.com>
2023-06-20 14:42:58 +0300

    Add RegisterRequest validator

---
+++ doska/Validators/RegisterRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using doska.DTO;
+using FluentValidation;
+
+namespace doska.Validators;
+
+internal sealed class RegisterRequestValidator : AbstractValidator<RegisterRequest>
+{
+    public RegisterRequestValidator()
+    {
+        RuleFor(request => request.FirstName)
+            .NotEmpty()
+            .MaximumLength(35);
+        RuleFor(request => request.LastName)
+            .NotEmpty()
+            .MaximumLength(35);
+        RuleFor(request => request.Email)
+            .EmailAddress();
+        RuleFor(request => request.Password)
+            .MinimumLength(6);
+    }
+}
