6ebbfc9f89fdde1aa7eecb55b43f1a441ddbd433
velvet_orange <ismediaspb@gmail.com>
2023-06-20 16:23:48 +0300

    Add UserInfoRequest validator

---
+++ doska/Validators/UserInfoRequestValidator.cs
@@ -0,0 +1,12 @@
+﻿using doska.DTO;
+using FluentValidation;
+
+namespace doska.Validators;
+
+internal sealed class UserInfoRequestValidator : AbstractValidator<UserInfoRequest>
+{
+    public UserInfoRequestValidator()
+    {
+        RuleFor(request => request.Email).EmailAddress();
+    }
+}
