84763eebffe56bd8bc8ef375c0160341a88e6a2d
velvet_orange <ismediaspb@gmail.com>
2023-06-30 17:09:33 +0300

    EditCategoryRequest, EditCategoryRequestValidator

---
+++ doska/DTO/EditCategoryRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace doska.DTO;
+
+public class EditCategoryRequest
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; } = default!;
+    public Guid? ParentId { get; set; }
+}
---
+++ doska/Validators/EditCategoryRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using doska.DTO;
+using doska.Services;
+using FluentValidation;
+
+namespace doska.Validators;
+
+public class EditCategoryRequestValidator : AbstractValidator<EditCategoryRequest>
+{
+    public EditCategoryRequestValidator(ICategoryService categoryService)
+    {
+        CascadeMode = CascadeMode.Stop;
+        RuleFor(request => request.Id)
+            .NotEmpty()
+            .MustAsync(async (id, ct) => await categoryService.ExistingCategory(id, ct))
+            .WithMessage("Category not found with given id");
+        RuleFor(request => request.Name)
+            .MinimumLength(4)
+            .MaximumLength(50)
+            .MustAsync(async (cat, ct) => await categoryService.CategoryNameExists(cat, ct))
+            .WithMessage("Category name already exists");
+    }
+}
