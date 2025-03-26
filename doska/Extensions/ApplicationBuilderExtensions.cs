800738296ff8607af0a7d1100a40c669c55bab38
ownr500 <ismediaspb@gmail.com>
2025-03-26 19:48:16 +0300

    update

--- doska/Extensions/ApplicationBuilderExtensions.cs
+++ doska/Extensions/ApplicationBuilderExtensions.cs
@@ -1,9 +1,11 @@
+using doska.Configurations;
+
 namespace doska.Extensions;
 
 internal static class ApplicationBuilderExtensions
 {
     public static void RegisterConfigurationOptions(this WebApplicationBuilder builder)
     {
-        
+        builder.Services.ConfigureOptions<ConfigureSwaggerOptions>();
     }
 }
