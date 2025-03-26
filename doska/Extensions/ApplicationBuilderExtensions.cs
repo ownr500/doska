983439bedcfb52780362d825843a3a64d815fc62
ownr500 <ismediaspb@gmail.com>
2025-03-26 17:36:21 +0300

    Introduce ApplicationBuilderExtensions

---
+++ doska/Extensions/ApplicationBuilderExtensions.cs
@@ -0,0 +1,9 @@
+namespace doska.Extensions;
+
+internal static class ApplicationBuilderExtensions
+{
+    public static void RegisterConfigurationOptions(this WebApplicationBuilder builder)
+    {
+        
+    }
+}
